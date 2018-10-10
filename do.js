5b25169b00f6f2e6d1cbc307611e478b613c2f80
stochastic-thread <hello@arthurcolle.com>
2018-10-10 13:17:50 -0400

    Droplet creation function. Setting unscoped var to droplet id so I can delete it really quick...

---
+++ do.js
@@ -0,0 +1,49 @@
+const auth_token = process.env.DO_TOKEN_CURRENT;
+
+const DigitalOcean = require('do-wrapper').default,
+    api = new DigitalOcean(auth_token, 1024);
+
+console.log(" -- Retrieving account info. -- ");
+api.account((err, res, body) => {
+  console.log(body);
+});
+
+console.log("\n");
+console.log("\n");
+console.log(" -- Creating a droplet. -- ");
+
+configuration={
+  "name": "example.com",
+  "region": "nyc3",
+  "size": "s-1vcpu-1gb",
+  "image": "ubuntu-16-04-x64",
+  "ssh_keys": null,
+  "backups": false,
+  "ipv6": true,
+  "user_data": null,
+  "private_networking": null,
+  "volumes": null,
+  "tags": [
+    "web"
+  ]
+}
+
+let id = null;
+
+api.dropletsCreate(configuration,(err, res, body) => {
+    console.log('err:');
+    console.log(err);
+    console.log('res:');
+    console.log(res);
+    console.log('body:');
+    console.log(body);
+
+    if(id === null){
+      id = body.droplet.id;
+    } else {
+      console.log(`id: #{id}`)
+    };
+});
+
+console.log(id);
+
--- src/js/access_env.js
+++ src/js/access_env.js
@@ -1,4 +1,15 @@
-const fs = require('fs');
 const process = require('process');
+const cURL = require('curl-request');
+
+const auth_token = process.env.DO_TOKEN_CURRENT;
+
+# There are different Objects that you can interact with via the Web API.
+# One such object is ACCOUNT.
+#
+# `account` example
+# curl -X $HTTP_METHOD -u "$(auth_token)" "https://api.digitalocean.com/v2/account"
+
+
+
 
 
